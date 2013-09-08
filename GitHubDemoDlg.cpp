66ec0be6911420bea70f7a7725eab286251a3dca
bbsteel <bjbbsteel@gmail.com>
2013-09-08 11:05:28 +0800

    Remove some line
    dlg.cpp

--- GitHubDemoDlg.cpp
+++ GitHubDemoDlg.cpp
@@ -97,7 +97,7 @@ BOOL CGitHubDemoDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 设置大图标
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
-	// TODO: 在此添加额外的初始化代码
+
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
